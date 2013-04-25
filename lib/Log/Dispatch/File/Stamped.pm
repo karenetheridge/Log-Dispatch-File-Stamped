4c99a9c4499c4c5ade68f19efaaff938bbdd0def
Karen Etheridge <ether@cpan.org>
2013-04-25 13:08:04 -0700

    bump version to 0.10_005

--- lib/Log/Dispatch/File/Stamped.pm
+++ lib/Log/Dispatch/File/Stamped.pm
@@ -10,7 +10,7 @@ use vars qw(@ISA $VERSION);
 use Log::Dispatch::File 2.38;
 @ISA = qw(Log::Dispatch::File);
 
-$VERSION = '0.10_004';      # for PAUSE
+$VERSION = '0.10_005';      # for PAUSE
 $VERSION = eval $VERSION;   # the real version (a string literal)
 
 use Params::Validate qw(validate SCALAR);
