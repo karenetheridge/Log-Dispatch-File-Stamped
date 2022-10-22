64ace4e56651da338929e86045a25480e984d124
Karen Etheridge <ether@cpan.org>
2022-10-22 11:28:23 -0700

    increment $VERSION after 0.20 release

--- lib/Log/Dispatch/File/Stamped.pm
+++ lib/Log/Dispatch/File/Stamped.pm
@@ -5,7 +5,7 @@ package Log::Dispatch::File::Stamped;
 # ABSTRACT: Logging to date/time stamped files
 # KEYWORDS: log logging output file timestamp date rolling rotate
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 use File::Basename        qw(fileparse);
 use File::Spec::Functions qw(catfile);
